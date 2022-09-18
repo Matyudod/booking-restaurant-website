6c368cc81fdeceaeeaec1e855bf008932e1f6d75
Matyudod <tamb1910291@student.ctu.edu.vn>
2022-09-18 10:50:53 +0700

    update

--- Web/src/app/controllers/comments.controller.js
+++ Web/src/app/controllers/comments.controller.js
@@ -18,5 +18,118 @@ const {
 } = require("../services/index.service");
 
 const commentService = new CommentService(models);
-class CommentController {}
+class CommentController {
+    async create(req, res) {
+        try {
+            let comment = {
+                bill_id: parseInt(req.body.name),
+                content: req.body.name,
+                point: parseInt(req.body.name),
+            };
+            const v = new Validator();
+            let validationResponse = v.validate(comment, scheme.commentCreateValidation);
+            if (validationResponse !== true) {
+                res.status(400).json(message.errorFieldIsNull);
+            } else {
+                let newComment = await commentService.create(comment);
+                if (newComment != null) {
+                    let error = message.createSuccessful;
+                    error.message = error.message.replace("{1}", "Comment");
+                    res.status(200).json(error);
+                } else {
+                    let error = message.errorFieldIsExisted;
+                    error.message = error.message.replace("{1}", "Comment");
+                    res.status(200).json(error);
+                }
+            }
+        } catch (err) {
+            res.status(500).json(message.APIErrorServer);
+        }
+    }
+
+    async getByBillId(req, res) {
+        try {
+            let bill_id = req.params.id ?? -1;
+            let commentWithBillId = {
+                bill_id: parseInt(bill_id),
+            };
+            const v = new Validator();
+            let validationResponse = v.validate(commentWithBillId, scheme.billIdValidation);
+            if (validationResponse !== true) {
+                res.status(400).json(message.errorIdFieldIsNull);
+            } else {
+                let comment = await commentService.getByBillId(commentWithBillId.id);
+                if (comment != null) {
+                    res.status(200).json(comment);
+                } else {
+                    let error = message.errorNotFound;
+                    error.message = error.message.replace("{1}", "Comment");
+                    res.status(400).json(message.errorNotFound);
+                }
+            }
+        } catch (error) {
+            res.status(500).json(message.APIErrorServer);
+        }
+    }
+
+
+    async update(req, res) {
+        try {
+            let id = req.params.id ?? -1;
+            let comment = {
+                bill_id: parseInt(req.body.name),
+                content: req.body.name,
+                point: parseInt(req.body.name),
+            };
+            const v = new Validator();
+            let validationResponse = v.validate(comment, scheme.typeOfPartyCreateValidation);
+            if (validationResponse !== true) {
+                res.status(400).json(message.errorFieldIsNull);
+            } else {
+                let isUpdated = await commentService.update(id, comment);
+                if (!isUpdated) {
+                    let errorNotFound = message.errorNotFound;
+                    errorNotFound.message = errorNotFound.message.replace("{1}", "Comment");
+                    res.status(200).json(errorNotFound);
+                } else {
+                    let updateSuccessful = message.updateSuccessful;
+                    updateSuccessful.message = updateSuccessful.message.replace(
+                        "{1}",
+                        "Comment"
+                    );
+                    res.status(200).json(updateSuccessful);
+                }
+            }
+        } catch (err) {
+            res.status(500).json(message.APIErrorServer);
+        }
+    }
+
+    async delete(req, res) {
+        try {
+            let id = req.params.id ?? -1;
+            let commnetId = {
+                id: parseInt(id),
+            };
+            const v = new Validator();
+            let validationResponse = v.validate(commnetId, scheme.idValidation);
+            if (validationResponse !== true) {
+                res.status(400).json(message.errorIdFieldIsNull);
+            } else {
+                let isDeleted = await commentService.delete(commnetId.id);
+                if (!isDeleted) {
+                    let errorNotFound = message.errorNotFound;
+                    errorNotFound.message = errorNotFound.message.replace("{1}", "Comment");
+                    res.status(200).json(errorNotFound);
+                } else {
+                    let deleteSuccessful = message.deleteSuccessful;
+                    deleteSuccessful.message = deleteSuccessful.message.replace("{1}", "Comment");
+                    res.status(200).json(deleteSuccessful);
+                }
+            }
+        } catch (error) {
+            res.status(500).json(message.APIErrorServer);
+        }
+    }
+}
 module.exports = new CommentController();
